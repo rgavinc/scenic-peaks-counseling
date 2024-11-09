8213bdd69bc57b8286e13006def15dd4249da644
Gavin Robertson <gavinrob92@gmail.com>
2024-11-09 00:01:11 -0700

    updates

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <h1>About Scenic Peaks Counseling</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+  </div>
+);
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1>Scenic Peaks Counseling</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+  </div>
+);
+
+export default Home;
