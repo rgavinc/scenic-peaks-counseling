10e6a6645ee994269068ad70c7f53ed533af329d
Gavin Robertson <gavinrob92@gmail.com>
2024-11-09 21:33:42 -0700

    added font, added mui, some styling changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#008080",
+    },
+  },
+  typography: {
+    fontFamily: "Thasadith, sans-serif",
+    fontSize: 14,
+    fontWeightRegular: 700,
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const Home = () => (
-  <div>
-    <h1>Scenic Peaks Counseling</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-  </div>
+  <Box
+    sx={{
+      maxWidth: "1280px",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      marginX: "auto",
+    }}
+  >
+    <Typography variant="h4">Scenic Peaks Counseling</Typography>
+    <Typography>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+    </Typography>
+  </Box>
 );
 
 export default Home;
