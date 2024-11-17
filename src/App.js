f5ebcd906d479c2e1eebc3aadd2fc4f25cc41577
Gavin Robertson <gavinrob92@gmail.com>
2024-11-17 00:15:03 -0700

    working on some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+  useNavigate,
+} from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
+import {
+  useMediaQuery,
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+  IconButton,
+} from "@mui/material";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { Box } from "@mui/material";
 import NotFound from "./pages/NotFound";
 
 const theme = createTheme({
@@ -20,27 +34,89 @@ const theme = createTheme({
   },
 });
 
+function Menu() {
+  const navigate = useNavigate();
+  const isXs = useMediaQuery("(max-width:650px)");
+
+  const menuItems = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Services", path: "/services" }, // Add Services page later
+    { name: "Contact", path: "/contact" }, // Add Contact page later
+  ];
+
+  return (
+    <AppBar position="static">
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="logo"
+          onClick={() => navigate("/")}
+          sx={{ marginRight: 2 }}
+        >
+          <img
+            src="/logo_no_text.svg"
+            alt="Logo"
+            style={{ width: 40, height: 40 }}
+          />
+          {!isXs && (
+            <Typography
+              variant="h6"
+              onClick={() => navigate("/")}
+              sx={{ flexGrow: 1, cursor: "pointer" }}
+            >
+              Scenic Peaks Counseling
+            </Typography>
+          )}
+        </IconButton>
+        <Box>
+          {menuItems.map((item) => (
+            <Button
+              key={item.name}
+              component={NavLink}
+              to={item.path}
+              color="inherit"
+              sx={{
+                "&.active": {
+                  borderBottom: "2px solid #ffffff",
+                },
+                marginLeft: 2,
+              }}
+            >
+              {item.name}
+            </Button>
+          ))}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Box
-        sx={{
-          maxWidth: "1280px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          marginX: "auto",
-        }}
-      >
-        <Router>
+      <Router>
+        <Menu />
+        <Box
+          sx={{
+            maxWidth: "1280px",
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginX: "auto",
+          }}
+        >
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </Router>
-      </Box>
+        </Box>
+      </Router>
     </ThemeProvider>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,25 @@
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import React from "react";
 
 const Home = () => (
-  <>
-    <Typography variant="h4">Scenic Peaks Counseling</Typography>
-    <Typography>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+  <Box
+    sx={{
+      width: "100%",
+      height: "100%",
+      backgroundImage: "url('/IMG_0888.jpg')",
+      backgroundSize: "cover", // Ensures the image covers the entire Box
+      backgroundPosition: "center", // Centers the image
+      backgroundRepeat: "no-repeat", // Prevents tiling of the image
+    }}
+  >
+    <Typography
+      variant="h3"
+      color="white"
+      sx={{ textAlign: "center", paddingTop: "100px" }}
+    >
+      Your Content Here
     </Typography>
-  </>
+  </Box>
 );
 
 export default Home;
